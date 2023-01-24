e97a101c40c2303489a4c8a87692fb0313ed0c59
Manuel <manuelortegagaliano@outlook.com>
2023-01-25 00:00:56 +0100

    fix problem title

--- .nuxt/dist/client/runtime.js
+++ .nuxt/dist/client/runtime.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0bd2d7aee551fb88d118";
+/******/ 	var hotCurrentHash = "083d7ce801e91573b0b2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/product/ProductDetailsWrapper.vue
+++ components/product/ProductDetailsWrapper.vue
@@ -247,5 +247,24 @@
             },
 
         },
+
+        head() {
+            return {
+                titleTemplate: this.product.name || 'Producto' + ' | TriviCare Natural Cosmetics',
+                link: [
+                    { rel: 'cannonical', href: 'https://trivicare.com/product/'}
+                ],
+                meta: [
+                    { charset: 'utf-8' },
+                    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                    {
+                        hid: 'description',
+                        name: 'description',
+                        content: this.product.meta_description
+                    }
+                ],
+
+            }
+        },
     };
 </script>
